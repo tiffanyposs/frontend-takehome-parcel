ed3f1898a8c669deeb843a4ccd2a6f2a7e468107
Tiffany Poss <tiffanyposs@gmail.com>
2019-02-19 22:25:28 -0500

    setup react, normalize, scss

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">App</div>
+    )
+  }
+}
+
+export default App;
