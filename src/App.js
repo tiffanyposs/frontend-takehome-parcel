7ceb3f8e62a37577d346ce3c1a636ff6322ef3db
Tiffany Poss <tiffanyposs@gmail.com>
2019-02-23 23:51:51 -0500

    add navbar and router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import DisplayGems from './components/DisplayGems';
-import Hero from './components/Hero';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+
+import HomePage from './components/pages/HomePage';
+import SavedGemPage from './components/pages/SavedGemPage';
+import Navbar from './components/Navbar';
 
 import './styles/App.scss';
 
@@ -9,8 +12,13 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container-fluid">
-          <Hero />
-          <DisplayGems />
+          <Router>
+            <div>
+              <Navbar />
+              <Route exact path='/' component={HomePage} />
+              <Route path='/saved' component={SavedGemPage} />
+            </div>
+          </Router>
         </div>
       </div>
     )
---
+++ src/components/pages/SavedGemPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DisplayGems from '../DisplayGems';
+import Hero from '../Hero';
+
+const SavedGemPage = () => {
+  return (
+    <div>
+      Hello world
+    </div>
+  )
+}
+
+export default SavedGemPage;
